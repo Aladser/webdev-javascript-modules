224acf2fd2b43753f9dcc11881fe09f3b1f69fcc
Aladser <aladser@gmail.com>
2022-11-10 22:45:03 +0900

    правка меню

--- M7_Calculator/script.js
+++ M7_Calculator/script.js
@@ -122,3 +122,15 @@ document.querySelector('#btn_sqrt').addEventListener('click', function () {
         last_sign = inputWindow.textContent[inputWindow.textContent.length-1];
     }
 })
+
+// кнопки меню
+let pages = document.querySelectorAll('header div');
+pages[0].addEventListener('click', function(){
+    window.open("../M6_TrafficLight/index.html", "_self");
+});
+pages[1].addEventListener('click', function(){
+    window.open("../index.html", "_self");
+});
+pages[2].addEventListener('click', function(){
+    window.open("../M8_GuessNumber/index.html", "_self");
+})
